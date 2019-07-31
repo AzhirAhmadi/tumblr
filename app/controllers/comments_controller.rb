34c8dac570697d2872fb4e47cf7397b1c67f738f
AzhirAhmadi <aaahmadi99@gmail.com>
2019-07-31 16:33:09 +0430

    -add delete to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,9 @@
 class CommentsController < ApplicationController
+    #before_action :find_post, only: [:find_comment]
+    before_action :find_comment, only: [:edit, :destroy, :update]
+    
+
+
     def create
         @post =Post.find(params[:post_id])
         @comment = @post.comments.create(comments_params)
@@ -6,8 +11,32 @@ class CommentsController < ApplicationController
         redirect_to post_path(@post)
     end
 
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+        @comment.destroy
+
+        redirect_to @post
+    end
+
+
+
+
 private
     def comments_params
         params.require(:comment).permit(:name, :body)
+    end
+
+    # def find_post
+    #     @post = Post.find(params[:post_id])
+    # end
+
+    def find_comment
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
     end
 end
